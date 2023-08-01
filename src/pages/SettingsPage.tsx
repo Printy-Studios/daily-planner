b0e59df0a855dc8567703cc5f9c13c9063b83d71
JorensM <jorensmerenjanu@gmail.com>
2023-08-01 10:48:08 +0300

    Basic font size input in settings

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,4 +1,8 @@
+//Core
+import { Formik, Form } from 'formik'
+
 //Components
+import Select from 'components/forms/Select'
 import Page from 'components/layout/Page'
 
 //Props
@@ -8,9 +12,40 @@ type Props = {}
  * Settings page. This is the top level page for the settings
  */
 export default function SettingsPage( {}: Props) {
+
+
+
     return (
         <Page>
-            Settings
+            <Formik
+                initialValues={{}}
+                onSubmit={() => {}}
+            >
+                <Form>
+                    <Select
+                        label='Font size'
+                        name='font-size'
+                        options={[
+                            {
+                                label: 'Small',
+                                value: 's'
+                            },
+                            {
+                                label: 'Regular',
+                                value: 'm'
+                            },
+                            {
+                                label: 'Large',
+                                value: 'l'
+                            },
+                            {
+                                label: 'Extra Large',
+                                value: 'xl'
+                            }
+                        ]}
+                    />
+                </Form>
+            </Formik>
         </Page>
     )
 }
