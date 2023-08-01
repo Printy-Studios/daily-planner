86b61a0385fb91f74e84027a11c8e86223186245
JorensM <jorensmerenjanu@gmail.com>
2023-08-01 10:27:44 +0300

    Created <SettingsPage/> component

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,16 @@
+//Components
+import Page from 'components/layout/Page'
+
+//Props
+type Props = {}
+
+/**
+ * Settings page. This is the top level page for the settings
+ */
+export default function SettingsPage( {}: Props) {
+    return (
+        <Page>
+            Settings
+        </Page>
+    )
+}
