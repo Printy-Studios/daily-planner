59ccf89376a2e2efd2b21e3ea34a756c582f069c
JorensM <jorensmerenjanu@gmail.com>
2023-09-13 13:33:03 +0300

    feat(forms): add header buttons to FormPage
    * additionally added an 'id' prop to FormPage and pass it to the Form component so the submit button works

--- src/components/layout/FormPage.tsx
+++ src/components/layout/FormPage.tsx
@@ -5,26 +5,39 @@ import { Formik, Form } from 'formik'
 //Components
 import Page from './Page'
 import FormAutoSave from 'components/misc/FormAutoSave'
+import BackButton from 'components/buttons/BackButton'
 
 type FormPageProps<FormValuesT> = {
+    id: string
     autoSave: false
     initialValues: FormValuesT
     onSubmit: (values: FormValuesT) => void
 }
 
 export default function FormPage<FormValuesT>( { 
+    id,
     children, 
     autoSave, 
     initialValues,
     onSubmit = () => {}
 }: PropsWithChildren<FormPageProps<FormValuesT>>) {
     return (
-        <Page>
+        <Page
+            headerLeft={
+                <BackButton />
+            }
+            headerRight={
+                autoSave ? null :
+                <button form={id} type='submit'>Save</button>
+            }
+        >
             <Formik
                 initialValues={initialValues}
                 onSubmit={onSubmit}
             >
-                <Form>
+                <Form
+                    id={id}
+                >
                     {/* Component to enable auto-save */}
                     { autoSave ? <FormAutoSave /> : null }{/* #TODO: add AutoSave component*/}
                     {/* Fields here */}
