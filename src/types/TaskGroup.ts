697b17505eb9721d5f771444fa984f88fffa75f1
JorensM <jorensmerenjanu@gmail.com>
2023-09-14 12:46:04 +0300

    feat(types): add variable types for `TaskGroup` type
    * add variable types such as TaskGroup with no id or TaskGroup with optional properties

--- src/types/TaskGroup.ts
+++ src/types/TaskGroup.ts
@@ -11,4 +11,13 @@ type TaskGroup = {
     color: string //Color of the task group as displayed in task group lists
 }
 
+type TaskGroupOptional = Omit<Partial<TaskGroup>, 'id'> & {id: ID}
+
+type TaskGroupNoID = Omit<TaskGroup, 'id'>
+
+export { 
+    type TaskGroupNoID, 
+    type TaskGroupOptional 
+}
+
 export default TaskGroup
