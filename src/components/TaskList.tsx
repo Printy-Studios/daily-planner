62d30cfa2bd15545be0fb94b94f734959af5059b
JorensM <jorensmerenjanu@gmail.com>
2024-01-08 09:35:35 +0200

    add comments for TaskList.tsx

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,20 +1,30 @@
+// #TODO change styles to semantic style
+// #TODO avoid inline styles if not used with a variable
+
+// Core
 import { useNavigate } from 'react-router-dom'
+
+// Types
 import { Task, TaskType } from 'types/Task'
 
 type TaskListItemProps = {
-    showDivider?: boolean
-    dividerColor: string
-    variant: 'dark' | 'light'
-    task: Task
+    showDivider?: boolean // Whether to show a divider between items
+    dividerColor: string // The color of the dicider
+    variant: 'dark' | 'light' // Color variant
+    task: Task // The task to display
 }
 
-
-
+/**
+ * Single item for the TaskList component
+ */
 function TaskListItem({ task, variant, dividerColor, showDivider = true }: TaskListItemProps) {
 
+    // Hooks
     const navigate = useNavigate()
 
+    // Called when task item is clicked (not the checkbox)
     const handleTaskClick = () => {
+        // Goto /task-edit and pass id of task as a param
         navigate('/task-edit', { state: {id: task.id} } )
     }
 
@@ -22,16 +32,17 @@ function TaskListItem({ task, variant, dividerColor, showDivider = true }: TaskL
         <div 
             className={
                 `task-list-item flex flex-row flex-wrap items-center gap-x-xs p-xxs p-l-s
-            `}
+            `} 
             style={{
-                borderBottom: showDivider ? `1px solid ${dividerColor}` : ''
+                borderBottom: showDivider ? `1px solid ${dividerColor}` : '' // Show divider if necessary
             }}
         >
+            {/* Checkbox/dot */}
             <div 
                 className='task-item-checkbox'
                 
             >
-                {task.type === TaskType.TASK ?
+                {task.type === TaskType.TASK ? // If task type is TASK, then display checkbox, otherwise display dot
                     <input 
                         type='checkbox' 
                         className='m-none'
@@ -45,7 +56,9 @@ function TaskListItem({ task, variant, dividerColor, showDivider = true }: TaskL
                     />
                 }
             </div>
+            {/* Task info (title/description) */}
             <div className='task-item-info' onClick={handleTaskClick}>
+                {/* Title */}
                 <span
                     className='flex border-box flex-grow'
                     style={{
@@ -56,7 +69,8 @@ function TaskListItem({ task, variant, dividerColor, showDivider = true }: TaskL
                     {task.name}
                 </span>
                 
-                {task.description ? 
+                {/* Description, if there is one */}
+                {task.description ?
                     <div 
                         className='w-full text-s text-gray-darker'
                         style={{
@@ -72,11 +86,14 @@ function TaskListItem({ task, variant, dividerColor, showDivider = true }: TaskL
 }
 
 type TaskListProps = {
-    tasks: Task[]
-    variant: 'dark' | 'light'
-    dividerColor: string
+    tasks: Task[] // Array of tasks to display
+    variant: 'dark' | 'light' // Color variant
+    dividerColor: string // Divider color
 }
 
+/**
+ * Shows a list of tasks that you can check off or click on to edit
+ */
 export default function TaskList( { tasks, variant = 'dark', dividerColor }: TaskListProps ) {
     return (
         <ul className='list-reset'>
